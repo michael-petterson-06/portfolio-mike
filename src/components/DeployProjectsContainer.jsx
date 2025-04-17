9ae8e21f5acfe2cd9624e74dafaaffa437e21753
Michael <michaelpetterson06.com.br>
2025-04-17 16:52:17 -0300

    Update projects session.

--- src/components/DeployProjectsContainer.jsx
+++ src/components/DeployProjectsContainer.jsx
@@ -1,27 +1,54 @@
-import { FaFilm, FaBlog, FaBookOpen, FaCoins } from "react-icons/fa";
+import { FaFilm, FaBullhorn, FaShoppingCart,  FaDumbbell, FaTools, FaTruckMoving,  FaBlog, FaBookOpen, FaCoins } from "react-icons/fa";
 import "../styles/components/deployprojectscontainer.sass";
 
+
 const deployedProjects = [
+
+  {
+    name: "Solução Estratégica",
+    url: "https://routetm.com/",
+    icon: <FaBullhorn id="icon-faBullhorn" />,
+  },
+
+  {
+    name: "TNT FIT",
+    url: "https://tntfitacademia.com.br/",
+    icon: <FaDumbbell id="icon-dumbbell" />,
+  },
+
+  {
+    name: "DR Car Services",
+    url: "https://lp.rdcarservices.com.br/",
+    icon: <FaTools id="icon-fatools" />,
+  },
+
+  {
+    name: "Anjun Express",
+    url: "https://anjunexpress.com.br/",
+    icon: <FaTruckMoving id="icon-truckMoving" />,
+  },
+
   {
     name: "Movies Lib",
     url: "https://michael-petterson-06.github.io/movies-lib/",
     icon: <FaFilm id="icon-movies" />,
   },
-  {
-    name: "Mini Blog",
-    url: "https://michael-petterson-06.github.io/mini-blog/",
-    icon: <FaBlog id="icon-blog" />,
-  },
-  {
-    name: "Dicionário",
-    url: "https://dictionary-anju.vercel.app/",
-    icon: <FaBookOpen id="icon-dictionary" />,
-  },
-  {
-    name: "Simulador de Rendimento",
-    url: "https://simulador-financeiro-silk.vercel.app/",
-    icon: <FaCoins id="icon-simulador" />,
-  }
+  // {
+  //   name: "Mini Blog",
+  //   url: "https://michael-petterson-06.github.io/mini-blog/",
+  //   icon: <FaBlog id="icon-blog" />,
+  // },
+  // {
+  //   name: "Dicionário",
+  //   url: "https://dictionary-anju.vercel.app/",
+  //   icon: <FaBookOpen id="icon-dictionary" />,
+  // },
+  // {
+  //   name: "Simulador de Rendimento",
+  //   url: "https://simulador-financeiro-silk.vercel.app/",
+  //   icon: <FaCoins id="icon-simulador" />,
+  // },
+  
 ];
 
 const DeployProjectsContainer = () => {
